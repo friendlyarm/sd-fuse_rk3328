5a706a8206178c795aba2ebd3d255126a5d252ac
Lawrence-Tang <tangzongsheng@gmail.com>
2021-03-05 15:17:49 +0800

    add test sciprt for kernel header deb

---
+++ test/test-build-kernel-header-deb.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -eu
+
+HTTP_SERVER=112.124.9.243
+KERNEL_URL=https://github.com/friendlyarm/kernel-rockchip
+KERNEL_BRANCH=nanopi-r2-v5.10.y
+
+# hack for me
+PCNAME=`hostname`
+if [ x"${PCNAME}" = x"tzs-i7pc" ]; then
+	HTTP_SERVER=192.168.1.9
+	KERNEL_URL=git@192.168.1.5:/devel/kernel/linux.git
+	KERNEL_BRANCH=nanopi-r2-v5.10.y
+fi
+
+# clean
+mkdir -p tmp
+sudo rm -rf tmp/*
+
+cd tmp
+git clone ../../.git sd-fuse_rk3328
+cd sd-fuse_rk3328
+if [ -f ../../friendlywrt-images.tgz ]; then
+	tar xvzf ../../friendlywrt-images.tgz
+else
+	wget http://${HTTP_SERVER}/dvdfiles/RK3328/images-for-eflasher/friendlywrt-images.tgz
+    tar xvzf friendlywrt-images.tgz
+fi
+
+if [ -f ../../kernel-rk3328.tgz ]; then
+	tar xvzf ../../kernel-rk3328.tgz
+else
+	git clone ${KERNEL_URL} --depth 1 -b ${KERNEL_BRANCH} kernel-rk3328
+fi
+
+MK_HEADERS_DEB=1 BUILD_THIRD_PARTY_DRIVER=0 KERNEL_SRC=$PWD/kernel-rk3328 ./build-kernel.sh friendlywrt
