9f24b6837d56b3c38be9a4e70b5c81593dd751f7
Lawrence-Tang <tangzongsheng@gmail.com>
2024-06-12 20:01:33 +0800

    build-kernel.sh: update paths and cleanup process

--- build-kernel.sh
+++ build-kernel.sh
@@ -110,14 +110,14 @@ if [ ! -d $OUT ]; then
     exit 1
 fi
 KMODULES_OUTDIR="${OUT}/output_${SOC}_kmodules"
-true ${kernel_src:=${OUT}/kernel-${SOC}}
-true ${KERNEL_SRC:=${kernel_src}}
+true ${kernel_src:=out/kernel-${SOC}}
+true ${KERNEL_SRC:=$(readlink -f ${kernel_src})}
 
 function usage() {
        echo "Usage: $0 <OS>"
        echo "# example:"
        echo "# clone kernel source from github:"
-       echo "    git clone ${KERNEL_REPO} --depth 1 -b ${KERNEL_BRANCH} ${KERNEL_SRC}"
+       echo "    git clone ${KERNEL_REPO} --depth 1 -b ${KERNEL_BRANCH} ${kernel_src}"
        echo "# or clone your local repo:"
        echo "    git clone git@192.168.1.2:/path/to/linux.git --depth 1 -b ${KERNEL_BRANCH} ${KERNEL_SRC}"
        echo "# then"
@@ -195,6 +195,7 @@ if [ ! -d ${KERNEL_SRC} ]; then
     git clone ${KERNEL_REPO} --depth 1 -b ${KERNEL_BRANCH} ${KERNEL_SRC}
 fi
 
+echo "kernel src: ${KERNEL_SRC}"
 if [ -f "${LOGO}" ]; then
     cp -f ${LOGO} ${KERNEL_SRC}/logo.bmp
     echo "using ${LOGO} as logo."
@@ -211,7 +212,7 @@ fi
 
 
 cd ${KERNEL_SRC}
-make distclean
+make CROSS_COMPILE=${CROSS_COMPILE} ARCH=${ARCH} distclean
 touch .scmversion
 make CROSS_COMPILE=${CROSS_COMPILE} ARCH=${ARCH} ${KCFG}
 if [ $? -ne 0 ]; then
@@ -324,6 +325,8 @@ fi
 if [ x"$DISABLE_MKIMG" = x"1" ]; then
     exit 0
 fi
+
+echo "building kernel ok."
 
 cd ${TOPPATH}
 download_img ${TARGET_OS}
