7ec871c83a852d3f1da3facbfdb23d801fae8d2b
Lawrence-Tang <tangzongsheng@gmail.com>
2024-09-03 13:50:06 +0800

    update firmware

--- tools/setup_boot_and_rootfs.sh
+++ tools/setup_boot_and_rootfs.sh
@@ -15,9 +15,14 @@ OUT=${PWD}/out
 UBOOT_DIR=$1
 KERNEL_DIR=$2
 BOOT_DIR=$3
-ROOTFS_DIR=$4
+ROOTFS_DIR=$(readlink -f $4)
 PREBUILT=$5
 TARGET_OS=$(echo ${6,,}|sed 's/\///g')
+
+HOST_ARCH=
+if uname -mpi | grep aarch64 >/dev/null; then
+    HOST_ARCH="aarch64/"
+fi
 
 # kernel bin
 KMODULES_OUTDIR="${OUT}/output_${SOC}_kmodules"
@@ -32,12 +37,8 @@ rm -rf ${ROOTFS_DIR}/lib/modules/*
 })
 
 # firmware
-cp -avf ${PREBUILT}/firmware/etc ${ROOTFS_DIR}/
-cp -avf ${PREBUILT}/firmware/system ${ROOTFS_DIR}/
-if [ -d ${ROOTFS_DIR}/lib ]; then
-    cp -avf ${PREBUILT}/firmware/usr/lib/* ${ROOTFS_DIR}/lib
-else
-    cp -avf ${PREBUILT}/firmware/usr ${ROOTFS_DIR}/
-fi
+(cd ${PREBUILT}/firmware && {
+	./install.sh ${ROOTFS_DIR}
+})
 
 exit 0
