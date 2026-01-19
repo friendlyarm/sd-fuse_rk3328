fe436586e80ce47bf4bfa09c1864343bb9568564
Lawrence-Tang <tangzongsheng@gmail.com>
2026-01-19 15:37:07 +0800

    debian core: bump to debian trixie

--- test/test-build-all-images.sh
+++ test/test-build-all-images.sh
@@ -17,8 +17,8 @@ cd sd-fuse_rk3328
 wget --no-proxy http://${HTTP_SERVER}/dvdfiles/RK3328/images-for-eflasher/ubuntu-noble-core-arm64-images.tgz
 tar xzf ubuntu-noble-core-arm64-images.tgz
 
-wget --no-proxy http://${HTTP_SERVER}/dvdfiles/RK3328/images-for-eflasher/debian-bookworm-core-arm64-images.tgz
-tar xzf debian-bookworm-core-arm64-images.tgz
+wget --no-proxy http://${HTTP_SERVER}/dvdfiles/RK3328/images-for-eflasher/debian-trixie-core-arm64-images.tgz
+tar xzf debian-trixie-core-arm64-images.tgz
 
 wget --no-proxy http://${HTTP_SERVER}/dvdfiles/RK3328/images-for-eflasher/openmediavault-arm64-images.tgz
 tar xzf openmediavault-arm64-images.tgz
@@ -36,8 +36,8 @@ tar xzf emmc-flasher-images.tgz
 ./mk-sd-image.sh ubuntu-noble-core-arm64
 ./mk-emmc-image.sh ubuntu-noble-core-arm64
 
-./mk-sd-image.sh debian-bookworm-core-arm64
-./mk-emmc-image.sh debian-bookworm-core-arm64
+./mk-sd-image.sh debian-trixie-core-arm64
+./mk-emmc-image.sh debian-trixie-core-arm64
 
 ./mk-sd-image.sh openmediavault-arm64
 ./mk-emmc-image.sh openmediavault-arm64
