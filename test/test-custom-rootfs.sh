e06eb5f24877f6538d28166e2e811f63dffb758c
Lawrence-Tang <tangzongsheng@gmail.com>
2026-07-15 18:10:42 +0800

    Migrate image downloads to CDN and switch verify from md5 to sha256

--- test/test-custom-rootfs.sh
+++ test/test-custom-rootfs.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -eu
 
-HTTP_SERVER=112.124.9.243
-
+if [ -f "$(dirname "$(readlink -f "$0")")/../.use-local-r2" ]; then
+    CDN_URL=http://cdn.local/friendlyelec-cdn/os-images/rk3328/images
+    ROOTFS_URL=http://cdn.local/friendlyelec-cdn/rootfs/rk3328
+else
+    CDN_URL=https://downloads.friendlyelec.com/os-images/rk3328/images
+    ROOTFS_URL=https://downloads.friendlyelec.com/rootfs/rk3328
+fi
 # hack for me
 [ -f /etc/friendlyarm ] && source /etc/friendlyarm $(basename $(builtin cd ..; pwd))
 
@@ -13,11 +18,13 @@ sudo rm -rf tmp/*
 cd tmp
 git clone ../../.git sd-fuse_rk3328
 cd sd-fuse_rk3328
-wget --no-proxy http://${HTTP_SERVER}/dvdfiles/RK3328/images-for-eflasher/ubuntu-noble-core-arm64-images.tgz
+wget ${CDN_URL}/ubuntu-noble-core-arm64-images.tgz
 tar xzf ubuntu-noble-core-arm64-images.tgz
-wget --no-proxy http://${HTTP_SERVER}/dvdfiles/RK3328/images-for-eflasher/emmc-flasher-images.tgz
+wget ${CDN_URL}/emmc-flasher-images.tgz
 tar xzf emmc-flasher-images.tgz
-wget --no-proxy http://${HTTP_SERVER}/dvdfiles/RK3328/rootfs/rootfs-ubuntu-noble-core-arm64.tgz
+wget ${ROOTFS_URL}/rootfs-ubuntu-noble-core-arm64.tgz
+wget ${ROOTFS_URL}/rootfs-ubuntu-noble-core-arm64.tgz.sha256
+sha256sum -c rootfs-ubuntu-noble-core-arm64.tgz.sha256
 
 sudo tar xzfp rootfs-ubuntu-noble-core-arm64.tgz --numeric-owner --same-owner
 sudo ./build-rootfs-img.sh ubuntu-noble-core-arm64/rootfs ubuntu-noble-core-arm64
